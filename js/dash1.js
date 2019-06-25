812123ac3f1e722eb4d36eaa630fd218b6b7d4c6
Dloft <derek.lofton1@gmail.com>
2019-06-25 12:13:54 -0400

    JSplots

--- js/dash1.js
+++ js/dash1.js
@@ -1,27 +1,45 @@
-
-// const newDash = 
-
-// $(document).ready(function(){
-//     $("img").click(function(){
-//         document.getElementById("manipulate").replaceWith("newDash");
-//     //   document.getElementById("manipulate").append(newDash);
-//     });
-//   });
-var testing = document.getElementById("plot1")
-var testing2 = document.getElementById("plot2")
-
-
 $(document).ready(function () {
 
+
+    var plot1 = document.getElementById("plot1")
+    var plot2 = document.getElementById("plot2")
+    var plot3 = document.getElementById("plot3")
+    var plot4 = document.getElementById("plot4")
+    var plot5 = document.getElementById("plot5")
+    var plot6 = document.getElementById("plot6")
+
+
+
     $("#plot1").click(function () {
-        $("#manipulate").append(testing);
-        console.log("clicked");
+        $("#manipulate").append(plot1);
+        console.log("testing1");
     });
 
     $("#plot2").click(function () {
-        $("#manipulate").append(testing2);
-        console.log("clicked");
+        $("#manipulate").append(plot2);
+        console.log("plot2");
     });
+
+    $("#plot3").click(function () {
+        $("#manipulate").append(plot3);
+        console.log("testing1");
+    });
+
+    $("#plot4").click(function () {
+        $("#manipulate").append(plot4);
+        console.log("plot2");
+    });
+
+    $("#plot5").click(function () {
+        $("#manipulate").append(plot5);
+        console.log("testing1");
+    });
+
+    $("#plot6").click(function () {
+        $("#manipulate").append(plot6);
+        console.log("plot2");
+    });
+
 });
 
 
